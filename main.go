51c9e6b1e56a8f773e3af908ce1890466dac4510
srijith13 <srijith31@gmail.com>
2024-01-09 22:47:23 +0530

    Initial commit need to fix everything

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+)
+
+//go:embed frontend/dist
+var assets embed.FS
+
+func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:            "country-api",
+		Width:            1024,
+		Height:           750,
+		Assets:           assets,
+		BackgroundColour: &options.RGBA{R: 245, G: 62, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err)
+	}
+}
